470c3be7c5d6453e0dfc6bbce22559007e9363d6
Mauro de Souza <maurodesouza2017@hotmail.com>
2022-07-21 17:51:41 -0300

    📔  class 13 - tests to video download and fragmenting

---
+++ application/services/video_service_test.go
@@ -0,0 +1,54 @@
+package services_test
+
+import (
+	"encoder/application/repositories"
+	"encoder/application/services"
+	"encoder/domain"
+	"encoder/framework/database"
+	"log"
+	"testing"
+	"time"
+
+	"github.com/joho/godotenv"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func init() {
+	err := godotenv.Load("../../.env")
+
+	if err != nil {
+		log.Fatalf("error loading .env file")
+	}
+}
+
+func prepare() (*domain.Video, repositories.VideoRepository) {
+	db := database.NewDbTest()
+
+	defer db.Close()
+
+	video := domain.NewVideo()
+
+	video.Id = uuid.NewV4().String()
+	video.FilePath = "video.mp4"
+	video.CreatedAt = time.Now()
+
+	repo := repositories.VideoRepository{Db: db}
+
+	return video, repo
+}
+
+func TestVideoServiceDownloadAndFragment(t *testing.T) {
+	video, repo := prepare()
+
+	videoService := services.NewVideoService()
+
+	videoService.Video = video
+	videoService.VideoRepository = repo
+
+	err := videoService.Download("microservice_encoder")
+	require.Nil(t, err)
+
+	err = videoService.Fragment()
+	require.Nil(t, err)
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	cloud.google.com/go/storage v1.9.0
 	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
 	github.com/jinzhu/gorm v1.9.16
+	github.com/joho/godotenv v1.4.0
 	github.com/lib/pq v1.1.1
 	github.com/satori/go.uuid v1.2.0
 	github.com/stretchr/testify v1.8.0
